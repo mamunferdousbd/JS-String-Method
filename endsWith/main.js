fecf80a9cfd28ff9ef5ffcd36212c79c5e8f9b0e
abdullah_mamun <aamun22@gmail.com>
2022-08-01 09:37:27 -0700

    string endsWith() method

---
+++ endsWith/main.js
@@ -0,0 +1,9 @@
+// JS String Method | endsWith() 
+// The endsWith() method returns true if a string ends with a specified string.
+// Otherwise it returns false.
+// The endsWith() method is case sensitive.
+var text ="Hello world, welcome to the universe.";
+var text= text.endsWith("universe.");
+
+// var text= text.endsWith("world",11);
+console.log(text);
