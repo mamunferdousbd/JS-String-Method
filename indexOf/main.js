853e5da1b874653e482eec5c06dc427f69299556
abdullah_mamun <aamun22@gmail.com>
2022-08-01 10:15:21 -0700

    string indexOf() method

---
+++ indexOf/main.js
@@ -0,0 +1,9 @@
+// JS String Method | indexOf() 
+
+// The indexOf() method returns the position of the first occurrence of a value in a string.
+// The indexOf() method returns -1 if the value is not found.
+// The indexOf() method is case sensitive.
+let text = "Hello world, welcome to the universe.";
+console.log(text.indexOf("welcome"));
+console.log(text.indexOf("Welcome"));
+
