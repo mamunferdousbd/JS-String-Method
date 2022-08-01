033b29e38ed186c3719e1fb1ab025dddcc9ac0f5
abdullah_mamun <aamun22@gmail.com>
2022-08-01 10:21:48 -0700

    string lastIndexOf method

---
+++ lastIndexOf/main.js
@@ -0,0 +1,11 @@
+// JS String Method | lastIndexOf() 
+
+// The lastIndexOf() method returns the index (position) of the last occurrence of a specified value in a string.
+// The lastIndexOf() method searches the string from the end to the beginning.
+// The lastIndexOf() method returns the index from the beginning (position 0).
+// The lastIndexOf() method returns -1 if the value is not found.
+// The lastIndexOf() method is case sensitive.
+let text = "Hello planet earth, you are a great planet.";
+console.log(text.lastIndexOf("planet"));
+console.log(text.lastIndexOf("Planet"));
+
