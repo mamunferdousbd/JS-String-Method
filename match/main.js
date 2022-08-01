db054ac310f9342dd0438f62bafeb5f338caa30e
abdullah-mamun <aamun22@gmail.com>
2022-08-01 21:22:39 +0300

    match method

--- match/main.js
+++ match/main.js
@@ -1,2 +1,8 @@
 // JS String Method | match()
 
+// The match() method matches a string against a regular expression **
+//The match() method returns an array with the matches.
+//The match() method returns null if no match is found.
+let text = "The rain in SPAIN stays mainly in the plain";
+let result = text.match("ain");
+console.log(result);
