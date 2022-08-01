ec1f9ca7a32f33b0513a4f97095df2544fe081f8
abdullah_mamun <aamun22@gmail.com>
2022-08-01 09:59:36 -0700

    String.fromCharCode is used for converting unicode to character

---
+++ String.fromCharCode/main.js
@@ -0,0 +1,11 @@
+// JS String Method | String.fromCharCode() 
+
+// The String.fromCharCode() method converts Unicode values to characters.
+// The String.fromCharCode() is a static method of the String object.
+//The syntax is always String.fromCharCode().
+// You cannot use myString.fromCharCode().
+
+
+var text = String.fromCharCode(65);
+var text = String.fromCharCode(65,66,68,85,76,76,65,72,65,76,77,65,77,85,78);
+console.log(text)
