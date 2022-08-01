58895f2ce5467d699e54f6bf1a060c8b788a5cc3
abdullah_mamun <aamun22@gmail.com>
2022-08-01 10:27:19 -0700

    string length property

---
+++ length/main.js
@@ -0,0 +1,9 @@
+// JS String Method | length property
+
+// The length property returns the length of a string.
+// The length property of an empty string is 0.
+let text = "Hello planet earth, you are a great planet.";
+console.log(text.length);
+let text1 ="";
+console.log(text1.length);
+
