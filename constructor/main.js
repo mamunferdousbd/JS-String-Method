0ce7cc09f263d5726892e52d77042b7bdaba8f28
abdullah_mamun <aamun22@gmail.com>
2022-08-01 09:27:31 -0700

    String constructor

---
+++ constructor/main.js
@@ -0,0 +1,9 @@
+// JS String Method | constractor 
+
+// The constructor property returns the function that created the String prototype.
+//For JavaScript strings the constructor returns:
+//function String() { [native code] }
+
+var text ="Abdullah Al Mamun";
+var text= text.constructor;
+console.log(text)
