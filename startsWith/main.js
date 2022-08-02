dde1a53700c42dfe6823ae7aceda0c4aa2863dbb
abdullah_mamun <aamun22@gmail.com>
2022-08-02 07:28:40 -0700

    Js string method startsWith()

---
+++ startsWith/main.js
@@ -0,0 +1,18 @@
+//JS String Method | startsWith()
+
+// The startsWith() method returns true if a string starts with a specified string.
+
+// Otherwise it returns false.
+
+// The startsWith() method is case sensitive.
+
+// Starts With Position 0
+let text = "Hello world, welcome to the universe.";
+let result = text.startsWith("Hello");
+console.log(result);
+
+// Starts With Position 6 
+let text1 = "Hello world, welcome to the universe.";
+let result1 = text.startsWith("world",6);
+console.log(result1);
+
