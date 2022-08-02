cae650f9fe6b6bcf6edb32a986cbc2a061e017ae
abdullah_mamun <aamun22@gmail.com>
2022-08-02 05:23:23 -0700

    details expalined

--- match/main.js
+++ match/main.js
@@ -3,6 +3,21 @@
 // The match() method matches a string against a regular expression **
 //The match() method returns an array with the matches.
 //The match() method returns null if no match is found.
-let text = "The rain in SPAIN stays mainly in the plain";
-let result = text.match("ain");
+
+// Using as a String
+var text = "The rain in SPAIN stays mainly in the plain";
+var result = text.match("ain");
+console.log(result);
+
+// Using as a using a regular expression:
+var text = "The rain in SPAIN stays mainly in the plain";
+var result = text.match(/ain/);
+console.log(result);
+// Using as a using a globl expression:
+var text = "The rain in SPAIN stays mainly in the plain";
+var result = text.match(/ain/g);
+console.log(result);
+// Using as a using a A global, case-insensitive search:
+var text = "The rain in SPAIN stays mainly in the plain";
+var result = text.match(/ain/gi);
 console.log(result);
