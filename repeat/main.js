3ecf295f1ded1426e4b755ef92360d3c0783a979
abdullah_mamun <aamun22@gmail.com>
2022-08-01 20:09:27 -0700

    js string method repeat()

---
+++ repeat/main.js
@@ -0,0 +1,14 @@
+//JS String Method | repeat()
+
+//The repeat() method returns a string with a number of copies of a string.
+//The repeat() method returns a new string.
+// The repeat() method does not change the original string.
+
+let text = "Hello world!";
+console.log(text);
+let result = text.repeat(2);
+let result1 = text.repeat(4);
+let result2 = text.repeat(6);
+console.log(result);
+console.log(result1);
+console.log(result2);
