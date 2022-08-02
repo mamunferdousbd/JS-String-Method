3e3a7fbee7f6b78a99f9d88c8dd6b517a5a06718
abdullah_mamun <aamun22@gmail.com>
2022-08-02 09:41:01 -0700

    toString Method

---
+++ toString/main.js
@@ -0,0 +1,19 @@
+// Js String Method | toString()
+
+// The toString() method returns a string as a string.
+// The toString() method does not change the original string.
+//The toString() method can be used to convert a string object into a string.
+
+//Get the content of the text:
+var text = "Hello World!";
+var result = text.toString();
+console.log(result);
+
+// Same as toString();
+var text = "Hello World!";
+var result = text;
+console.log(result);
+// Get the value of a String object:
+var text = new String("Hello World!");
+var result = text.toString();
+console.log(result);
